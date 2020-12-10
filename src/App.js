10d89064f767d3f9c6a3b1e7fb64e2f51d4f17db
Even_123 <1536841537@qq.com>
2020-12-10 18:15:04 +0800

    添加login和admin路由

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React,{Component} from 'react'
-import { Button } from 'antd';
-// import { DownloadOutlined } from '@ant-design/icons';
+import {BrowserRouter,Route,Switch} from 'react-router-dom'
+
+import Login from './pages/login/login'
+import Admin from './pages/admin/admin'
 export default class App extends Component{
   render () {
-    return <div>
-       <Button type="primary">primary</Button>
-       <Button type="dashed">Dashed Button</Button>
-    </div>
+    return (
+      //配置路由 BrowserRouter为路径，不带#
+      //Switch 只显示一个标签
+      <BrowserRouter>
+        <Switch>
+          <Route path='/login' component={Login}></Route>
+          <Route path='/' component={Admin}></Route>
+        </Switch>
+       
+      </BrowserRouter>
+    )
   }
 }
