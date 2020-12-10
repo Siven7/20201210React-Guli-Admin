803515a10f214428b1cd5d0eb4d1c9a5888baeb3
Even_123 <1536841537@qq.com>
2020-12-10 17:37:38 +0800

    修改antd样式配置

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React,{Component} from 'react'
-
+import { Button } from 'antd';
+// import { DownloadOutlined } from '@ant-design/icons';
 export default class App extends Component{
   render () {
-    return <div>App</div>
+    return <div>
+       <Button type="primary">primary</Button>
+       <Button type="dashed">Dashed Button</Button>
+    </div>
   }
 }
